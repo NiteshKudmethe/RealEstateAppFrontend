9214815d9134dbf8c5b86be7c767b8a4a6510064
nitesh kudmethe <niteshkudmethe@niteshs-MacBook-Pro.local>
2023-07-31 12:15:18 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PropertyOwnersComponent } from './property-owners/property-owners.component';
 import { PropertiesComponent } from './properties/properties.component';
 import { TenantsComponent } from './tenants/tenants.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { PropertyAddComponent } from './property-add/property-add.component';
+import { PropertyOwnerComponent } from './property-owner/property-owner.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'add-property', component: PropertyAddComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'property-owner', component: PropertyOwnerComponent },
   { path: 'property-owners', component: PropertyOwnersComponent },
   { path: 'properties', component: PropertiesComponent },
   { path: 'tenants', component: TenantsComponent },
--- src/app/tenants/tenants.component.ts
+++ src/app/tenants/tenants.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tenant } from '../tenant.model';
+import { TenantService } from '../tenant.service';
+
+
 
 @Component({
   selector: 'app-tenants',
   templateUrl: './tenants.component.html',
   styleUrls: ['./tenants.component.css']
 })
-export class TenantsComponent {
+export class TenantsComponent implements OnInit {
+  tenants: Tenant[] = [];
 
+  constructor(private tenantService: TenantService) { }
+
+  ngOnInit() {
+    this.loadTenants();
+  }
+
+  loadTenants() {
+    this.tenantService.getTenants().subscribe(
+      (tenants: Tenant[]) => {
+        this.tenants = tenants;
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 }
